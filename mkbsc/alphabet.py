1d72e8d576cc9a0b15091bf42f7dacd72918f50b
HelmerNylen <helmer@nylen.info>
2022-05-23 19:40:26 +0200

    Format alphabet.py

--- mkbsc/alphabet.py
+++ mkbsc/alphabet.py
@@ -4,38 +4,42 @@ from .helper_functions import _permute
 
 T = TypeVar("T")
 
+
 class Alphabet(Generic[T]):
-    """Represents the possible joint actions of the player coalition"""
-    def __init__(self, *actions: Iterable[T]):
-        """Generate a joint alphabet
+	"""Represents the possible joint actions of the player coalition"""
 
-        ex. a = Alphabet(('push', 'wait'), ('break', 'wait'))"""
-        
-        for playeractions in actions:
-            s = set()
-            for action in playeractions:
-                assert action not in s
-                s.add(action)
-        
-        self.actions = tuple(tuple(a for a in playeractions) for playeractions in actions)
+	def __init__(self, *actions: Iterable[T]):
+		"""Generate a joint alphabet
 
-    def __getitem__(self, index: int) -> Tuple[T, ...]:
-        """Get the alphabet (a tuple of the possible actions) for the specified player"""
-        return self.actions[index]
+        ex. a = Alphabet(('push', 'wait'), ('break', 'wait'))
+        """
 
-    def __iter__(self) -> Iterator[Tuple[T, ...]]:
-        """Iterate over the individual alphabets"""
-        for playeralphabet in self.actions:
-            yield playeralphabet
+		for playeractions in actions:
+			s = set()
+			for action in playeractions:
+				assert action not in s
+				s.add(action)
 
-    def __len__(self) -> int:
-        """Get the number of individual alphabets"""
-        return len(self.actions)
+		self.actions = tuple(
+		  tuple(a for a in playeractions) for playeractions in actions)
 
-    def __str__(self) -> str:
-        return str(self.actions)
-        
-    def permute(self) -> Iterator[Tuple[T, ...]]:
-        """Generate every possible joint action"""
-        for joint_action in _permute(self.actions):
-            yield joint_action
+	def __getitem__(self, index: int) -> Tuple[T, ...]:
+		"""Get the alphabet (a tuple of the possible actions) for the specified player"""
+		return self.actions[index]
+
+	def __iter__(self) -> Iterator[Tuple[T, ...]]:
+		"""Iterate over the individual alphabets"""
+		for playeralphabet in self.actions:
+			yield playeralphabet
+
+	def __len__(self) -> int:
+		"""Get the number of individual alphabets"""
+		return len(self.actions)
+
+	def __str__(self) -> str:
+		return str(self.actions)
+
+	def permute(self) -> Iterator[Tuple[T, ...]]:
+		"""Generate every possible joint action"""
+		for joint_action in _permute(self.actions):
+			yield joint_action
