d1ed08f37785f892742bfadd93a4cb324f17fea7
HelmerNylen <helmer@nylen.info>
2022-05-23 19:39:14 +0200

    Format transition.py

--- mkbsc/transition.py
+++ mkbsc/transition.py
@@ -2,30 +2,31 @@ from typing import Iterable, TypeVar
 
 from mkbsc.state import State
 
-
 T = TypeVar("T")
 
+
 class Transition:
-    """Represents a transition between two states"""
-    def __init__(self, start: State, joint_action: Iterable[T], end: State):
-        """Create a new transition"""
-        self.start = start
-        self.joint_action = tuple(joint_action)
-        self.end = end
+	"""Represents a transition between two states"""
 
-    def __getitem__(self, index: int) -> T:
-        """Get the action of a certain player"""
-        return self.joint_action[index]
+	def __init__(self, start: State, joint_action: Iterable[T], end: State):
+		"""Create a new transition"""
+		self.start = start
+		self.joint_action = tuple(joint_action)
+		self.end = end
 
-    def __repr__(self) -> str:
-        return str(self)
+	def __getitem__(self, index: int) -> T:
+		"""Get the action of a certain player"""
+		return self.joint_action[index]
 
-    def __str__(self) -> str:
-        return str(self.start) + " --" + str(self.joint_action) + "-> " + str(self.end)
-        
-    def label(self) -> str:
-        """Return the string representation of the joint action"""
-        if len(self.joint_action) > 1:
-            return "(" + ", ".join(str(action) for action in self.joint_action) + ")"
-        else:
-            return str(self.joint_action[0])
+	def __repr__(self) -> str:
+		return str(self)
+
+	def __str__(self) -> str:
+		return f"{self.start} --{self.joint_action}-> {self.end}"
+
+	def label(self) -> str:
+		"""Return the string representation of the joint action"""
+		if len(self.joint_action) > 1:
+			return "(" + ", ".join(str(action) for action in self.joint_action) + ")"
+		else:
+			return str(self.joint_action[0])
